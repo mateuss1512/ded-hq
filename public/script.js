cc33109414b3c436dd1ca80019c93890500ba654
Mateus <mateuscheytac@gmail.com>
2023-05-16 00:45:50 -0300

    Adicionado o login e registro por email e senha e corrigido o login por google

--- public/script.js
+++ public/script.js
@@ -1,17 +1,49 @@
-function smoothScroll(target) {
-    const element = document.querySelector(target);
-    window.scrollTo({
-      top: element.offsetTop,
-      behavior: 'smooth'
+const firebaseConfig = {
+        apiKey: "AIzaSyACCL9rEfx476t-FMAcNvyrI6UUv3VpiSY",
+        authDomain: "ded-hq-7288b.firebaseapp.com",
+        databaseURL: "https://ded-hq-7288b-default-rtdb.firebaseio.com",
+        projectId: "ded-hq-7288b",
+        storageBucket: "ded-hq-7288b.appspot.com",
+        messagingSenderId: "949359359975",
+        appId: "1:949359359975:web:d8ecf544075c739a42ef42",
+        measurementId: "G-9F8MKPXPV7",
+      };
+
+      firebase.initializeApp(firebaseConfig);
+
+      var db = firebase.firestore();
+      var auth = firebase.auth();
+
+     
+// Registrar usuário
+function registrarUsuario() {
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('password').value;
+  var msg = document.getElementById('message');
+  firebase.auth().createUserWithEmailAndPassword(email, senha)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      db.collection('emails').doc(user.uid).set({
+        email: user.email
+      })
+      .then(() => {
+        msg.style.color = "green";
+        msg.textContent = ("Usuário registrado com sucesso!");
+        document.getElementById('password').value = '';
+        document.getElementById('email').value = '';
+        redirecionar();
+      })
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      msg.style.color = "red";
+      msg.textContent = ("Erro ao registrar usuário:", errorCode, errorMessage);
     });
-  }
-  
-  const navLinks = document.querySelectorAll('nav ul li a');
-  navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault(); 
-      const target = link.getAttribute('href'); 
-      smoothScroll(target); 
-    });
-  });
-  
+    
+}
+
+ function redirecionar() {
+        window.location.href = "login.html";
+      }
+
--- public/scriptL.js
+++ public/scriptL.js
@@ -1,36 +1,72 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACCL9rEfx476t-FMAcNvyrI6UUv3VpiSY",
+  authDomain: "ded-hq-7288b.firebaseapp.com",
+  databaseURL: "https://ded-hq-7288b-default-rtdb.firebaseio.com",
+  projectId: "ded-hq-7288b",
+  storageBucket: "ded-hq-7288b.appspot.com",
+  messagingSenderId: "949359359975",
+  appId: "1:949359359975:web:d8ecf544075c739a42ef42",
+  measurementId: "G-9F8MKPXPV7",
+};
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyACCL9rEfx476t-FMAcNvyrI6UUv3VpiSY",
-//     authDomain: "ded-hq-7288b.firebaseapp.com",
-//     projectId: "ded-hq-7288b",
-//     storageBucket: "ded-hq-7288b.appspot.com",
-//     messagingSenderId: "949359359975",
-//     appId: "1:949359359975:web:d8ecf544075c739a42ef42",
-//     measurementId: "G-9F8MKPXPV7"
-// };
+firebase.initializeApp(firebaseConfig);
 
-// firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
 
-// const loginForm = document.getElementById('login-form');
-// const emailInput = document.getElementById('email');
-// const passwordInput = document.getElementById('password');
-// const messageElement = document.getElementById('message');
+var database = firebase.database();
 
-// loginForm.addEventListener('submit', (e) => {
-//   e.preventDefault(); 
+const provider = new firebase.auth.GoogleAuthProvider();
 
-//   const email = emailInput.value;
-//   const password = passwordInput.value;
+// Função que será chamada quando o botão de login com Google for clicado
+function loginWithGoogle() {
+    // Abre o pop-up de autenticação do Google
+    firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+            // O usuário foi autenticado com sucesso
+            const user = result.user;
+            console.log("Usuário autenticado:", user);
+            database.ref('users/' + user.uid).set({
+                email: user.email,
+                displayName: user.displayName
+            });
+            localStorage.setItem("chave",""+user.uid);
 
-//   firebase.auth().signInWithEmailAndPassword(email, password)
-//     .then((userCredential) => {
-//       const user = userCredential.user;
-//       messageElement.textContent = `Usuário autenticado com sucesso! UID: ${user.uid}`;
-//       messageElement.style.color = 'green';
-//     })
-//     .catch((error) => {
-//       const errorMessage = error.message;
-//       messageElement.textContent = `Erro ao autenticar usuário: ${errorMessage}`;
-//       messageElement.style.color = 'red';
-//     });
-// });
+            // Redireciona para a página index.html
+            redirecionarParaIndex();
+        })
+        .catch((error) => {
+            // Ocorreu um erro na autenticação
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Erro ao autenticar o usuário:", errorCode, errorMessage);
+        });
+}
+
+
+function redirecionarParaIndex() {
+    window.location.href = "principal.html";
+}
+
+const loginForm = document.getElementById('login-form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const messageElement = document.getElementById('message');
+
+function validar() {
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      messageElement.textContent = `Usuário autenticado com sucesso! UID: ${user.uid}`;
+      messageElement.style.color = 'green';
+      window.location.href = "principal.html";
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      messageElement.textContent = `Erro ao autenticar usuário: ${errorMessage}`;
+      messageElement.style.color = 'red';
+    });
+}
