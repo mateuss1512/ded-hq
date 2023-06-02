fb1f2945a8ce0716dde193cd5d991993a8cb6c2f
Mateus <mateuscheytac@gmail.com>
2023-06-02 18:08:55 -0300

    Implementado a geração de imagem por input de texto

--- public/perfil.js
+++ public/perfil.js
@@ -47,6 +47,19 @@ function print(usersDocRef) {
         raca.innerHTML = ntxt3;
         exp.innerHTML = ntxt4;
 
+        if (userData.hasOwnProperty('profile')) {
+          const imageElement = document.getElementById('profile-img');
+          imageElement.src = userData.profile;
+          document.getElementById('label').style.display = 'none';
+          document.getElementById('profile-input').style.display = 'none';
+          document.getElementById('btn1').style.display = 'none';
+          document.getElementById('btn2').style.display = 'none';
+        } else {
+          const defaultProfileImage = 'https://static.wikia.nocookie.net/3c9929a3-0314-4f3b-b3a6-e195d264bc05';
+          const imageElement = document.getElementById('profile-img');
+          imageElement.src = defaultProfileImage;
+        }
+
       } else {
         console.log("Nome de usuário não encontrado.");
       }
@@ -54,5 +67,52 @@ function print(usersDocRef) {
     .catch((error) => {
       console.log("Erro ao recuperar o nome do usuário:", error);
     });
+}
+
+const accessKey = 'XaEKMY_1xTJ4O_ZnlJYArLpORUeZ7wW3LrRReAyC4_E';
+const apiUrl = 'https://api.unsplash.com/photos/random';
+var generatedImg;
+
+async function gerarImagem(inputText) {
+  const response = await fetch(`${apiUrl}?query=${encodeURIComponent(inputText)}`, {
+    headers: {
+      Authorization: `Client-ID ${accessKey}`,
+    },
+  });
+
+  const data = await response.json();
+
+  if (data.errors) {
+    console.error('Erro ao obter a imagem:', data.errors);
+    return;
+  }
+
+  const imageUrl = data.urls.regular;
+  generatedImg = imageUrl;
+
+  const imageElement = document.getElementById('profile-img');
+  imageElement.src = imageUrl;
+}
+
+function salvar() {
+  const myCollection = firestore.collection("users").doc(userId);
+
+  myCollection.update({
+    profile: generatedImg,
+  });
+
+  document.getElementById('label').style.display = 'none';
+  document.getElementById('profile-input').style.display = 'none';
+  document.getElementById('btn1').style.display = 'none';
+  document.getElementById('btn2').style.display = 'none';
+  document.getElementById('btn3').style.display = 'block';
+}
+
+function atualizarImg() {
+  document.getElementById('label').style.display = 'block';
+  document.getElementById('profile-input').style.display = 'block';
+  document.getElementById('btn1').style.display = 'block';
+  document.getElementById('btn2').style.display = 'block';
+  document.getElementById('btn3').style.display = 'none';
 }
 
